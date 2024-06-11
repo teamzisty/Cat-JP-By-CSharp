295d6ae026260d016fea2843b8ac29ece29697a4
Syobosyobonn <154320513+syobosyobonn@users.noreply.github.com>
2024-06-11 19:36:25 +0900

    Upload files

---
+++ Cat-JP/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Cat_JP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 1)
+            {
+                Read(args[0]);
+            }
+            else
+            {
+                Console.WriteLine("正しくパスを入力してね");
+            }
+        }
+        public static void Read(string path)
+        {
+            string Text = "";
+            Console.WriteLine();
+            try
+            {
+                Text = File.ReadAllText(path);
+                Console.WriteLine("■パス=" + path);
+                Console.WriteLine("----------------------------------------------------------------------------");
+            }
+            catch
+            {
+                Console.WriteLine("パスが見当たんないよ");
+            }
+            Console.Write(Text);
+            Console.WriteLine();
+            Console.WriteLine("----------------------------------------------------------------------------");
+            Console.WriteLine("終了するにはなにかキーを押してください....");
+            Console.Read();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,34 @@
+# Cat-JP for C#
+## 概要
+このファイルはcatコマンドの日本語対応バージョンみたいなもの
+
+のVisual Studio のプロジェクトファイルです。
+## ビルド
+まず、Visual Studio(以後VS)でCat-JP.sinを開き、そのままビルドします。
+
+すると、実行ファイルが、\Cat-JP\Cat-JP\bin\Debug\に以下のように生成されます。
+```
+Debug
+┣Cat-JP.exe
+┣Cat-JP.exe.config
+┣Cat-JP.pdb
+```
+そのうちの"Cat-JP.exe"を任意の場所にコピーすることにより使用することをお勧めします。
+
+なお、"Cat-JP.exe.config","Cat-JP.pdb"は消去しても差し支えありません。
+## 使い方
+このプログラムは以下のようにして使用します
+```
+.\Cat-JP.exe {ファイル名}
+```
+すると、ファイルの中身がパスと共に表示されます。
+
+また、終了する際には何かキーを押すことによって終了できます。
+
+なお、毎回毎回実行ファイルのパスを指定するのが面倒な場合は、
+
+System32などに実行ファイルを置いたり、環境変数の設定をするとよいです。
+## 作者
+Syobosyobonn
+
+https://lit.link/Syobosyobonn
